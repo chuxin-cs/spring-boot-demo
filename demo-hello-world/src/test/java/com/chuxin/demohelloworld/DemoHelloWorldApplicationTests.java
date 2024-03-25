6d8d9db9071f8a513df148e77eb5ee59865fabf1
chuxin <15115359055@139.com>
2024-03-25 21:10:26 +0800

    feat: add spring-boot-demo/demo-hello-world

---
+++ demo-hello-world/src/test/java/com/chuxin/demohelloworld/DemoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuxin.demohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
