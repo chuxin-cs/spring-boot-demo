d827ee39912f4d82d634fd7d0dcf402eeea28363
chuxin <15115359055@139.com>
2024-06-04 16:42:55 +0800

    feat: 删除之前学习的文件目录结构重新开始

--- demo-curd/src/main/java/com/chuxin/democurd/config/SwaggerConfig.java
+++
@@ -1,4 +0,0 @@
-package com.chuxin.democurd.config;
-
-public class SwaggerConfig {
-}
--- demo-todo-list/src/main/java/com/chuxin/demotodolist/service/TodoService.java
+++
@@ -1,9 +0,0 @@
-package com.chuxin.demotodolist.service;
-
-import com.chuxin.demotodolist.entity.Todo;
-
-import java.util.List;
-
-public interface TodoService {
-    List<Todo> findAll();
-}
