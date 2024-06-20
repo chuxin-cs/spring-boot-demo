0b958d85a0de7163bcd443cef73b3cebba8f58ec
chuxin <15115359055@139.com>
2024-06-20 14:35:29 +0800

    feat: 用于 第02期：创建 SpringBoot 项目 的代码演示

---
+++ demo-env/src/main/java/com/chuxin/demoenv/DemoEnvApplication.java
@@ -0,0 +1,13 @@
+package com.chuxin.demoenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEnvApplication.class, args);
+    }
+
+}
