cc3cb0dcc4b4259628faa68a721cf130f81ca2a3
chuxin <15115359055@139.com>
2024-06-21 16:28:21 +0800

    feat: 用于 第04期：demo-crud 实现增删改查 代码演示

---
+++ demo-crud/src/main/java/com/chuxin/democrud/DemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.chuxin.democrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudApplication.class, args);
+    }
+
+}
---
+++ demo-crud/src/test/java/com/chuxin/democrud/DemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuxin.democrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
