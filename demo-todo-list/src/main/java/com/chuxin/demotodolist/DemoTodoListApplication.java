347ddc270aec0c741e681dd21c11b29c46f782ae
chuxin <15115359055@139.com>
2024-06-21 18:04:36 +0800

    feat: 用于 第05期：demo-todo-list 实现数据库增删改查 代码学习

---
+++ demo-todo-list/src/main/java/com/chuxin/demotodolist/DemoTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.chuxin.demotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTodoListApplication.class, args);
+    }
+
+}
---
+++ demo-todo-list/src/main/java/com/chuxin/demotodolist/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.chuxin.demotodolist.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    // 增加
+    @PostMapping("/add")
+    public String add() {
+        return "add";
+    }
+
+    // 删除
+    @PostMapping("/delete")
+    public String delete() {
+        return "delete";
+    }
+
+    // 修改
+    @PostMapping("/edit")
+    public String edit() {
+        return "edit";
+    }
+
+    // 查看
+    @GetMapping("/getList")
+    public String getList() {
+        return "getList";
+    }
+
+}
