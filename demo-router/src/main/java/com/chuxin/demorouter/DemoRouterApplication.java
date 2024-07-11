68853713d8b0e341190d24b294041e7b26dc342b
chuxin <15115359055@139.com>
2024-07-11 12:28:37 +0800

    feat: 增加注释

--- demo-router/src/main/java/com/chuxin/demorouter/DemoRouterApplication.java
+++ demo-router/src/main/java/com/chuxin/demorouter/DemoRouterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoRouterApplication {
 
+	// 入口文件
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRouterApplication.class, args);
 	}
