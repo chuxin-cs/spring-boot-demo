84ab6f66e5883f84931dc4ca49f56c69acdc6020
chuxin <15115359055@139.com>
2024-07-15 15:57:25 +0800

    feat: 用于 第09期：Spring Boot 环境变量 代码演示

--- demo-local/src/main/java/com/chuxin/demolocal/controller/LocalController.java
+++ demo-local/src/main/java/com/chuxin/demolocal/controller/LocalController.java
@@ -1,5 +1,6 @@
 package com.chuxin.demolocal.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/local")
 public class LocalController {
 
+    @Value("${spring.profiles.active}")
+    private String name;
+
     @GetMapping("/test")
     public String test() {
-        return "项目现在的环境是:" + "dev";
+        return "项目环境: " + name;
     }
 
 }
