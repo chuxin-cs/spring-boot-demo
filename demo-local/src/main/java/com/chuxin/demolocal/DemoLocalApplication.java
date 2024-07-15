591e94e1622cf38048d5a92aa5fa23ded8e912d6
chuxin <15115359055@139.com>
2024-07-15 15:45:42 +0800

    feat: 用于 第09期：Spring Boot 环境变量 代码演示

---
+++ demo-local/src/main/java/com/chuxin/demolocal/DemoLocalApplication.java
@@ -0,0 +1,13 @@
+package com.chuxin.demolocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLocalApplication.class, args);
+    }
+
+}
