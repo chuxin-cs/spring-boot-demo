4f1fbac4dbb8f220fd8983018a42e2963fa8aedb
chuxin <15115359055@139.com>
2024-07-23 16:22:04 +0800

    feat: 用于 第10期：SpringBoot 3 整合接口文档，包含Knife4j、Apifox~ 代码演示

--- demo-todo-list/src/main/java/com/chuxin/demotodolist/controller/UserController.java
+++ demo-todo-list/src/main/java/com/chuxin/demotodolist/controller/UserController.java
@@ -3,16 +3,32 @@ package com.chuxin.demotodolist.controller;
 import com.chuxin.demotodolist.common.result.Result;
 import com.chuxin.demotodolist.entity.User;
 import com.chuxin.demotodolist.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "body参数")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    // 查看  (用来演示 接口文档的)
+    @GetMapping("/getList1")
+    @Operation(summary = "获取用户列表")
+    @Parameters({
+            @Parameter(name = "page", description = "页数"),
+            @Parameter(name = "pageSize", description = "页码"),
+    })
+    public Result<List<User>> getList1(@RequestParam int page, @RequestParam int pageSize) {
+        return userService.getList(page, pageSize);
+    }
 
     // 测试
     @GetMapping("/failedTest")
@@ -47,6 +63,4 @@ public class UserController {
     public Result<List<User>> getList(@RequestParam int page, @RequestParam int pageSize) {
         return userService.getList(page, pageSize);
     }
-
-
 }
--- demo-todo-list/src/main/java/com/chuxin/demotodolist/entity/User.java
+++ demo-todo-list/src/main/java/com/chuxin/demotodolist/entity/User.java
@@ -1,5 +1,6 @@
 package com.chuxin.demotodolist.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Schema(description = "用户id")
     private int id;
+    @Schema(description = "用户名称")
     private String name;
+    @Schema(description = "用户密码")
     private String password;
 }
