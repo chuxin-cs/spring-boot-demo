9b3a5ac91bd13a8d18ef7af125387beaca18b5be
chuxin <15115359055@139.com>
2024-10-10 17:36:02 +0800

    feat: 启动类增加注解 和 resources 增加内容

--- demo-youlai/src/main/java/com/chuxin/demoyoulai/DemoYoulaiApplication.java
+++ demo-youlai/src/main/java/com/chuxin/demoyoulai/DemoYoulaiApplication.java
@@ -2,8 +2,12 @@ package com.chuxin.demoyoulai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableScheduling
 public class DemoYoulaiApplication {
 
     public static void main(String[] args) {
--- demo-youlai/src/main/java/com/chuxin/demoyoulai/mapper/UserMapper.java
+++ demo-youlai/src/main/java/com/chuxin/demoyoulai/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.chuxin.demoyoulai.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.chuxin.demoyoulai.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
