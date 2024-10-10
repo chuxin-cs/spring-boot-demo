22abe641de917deb7f962f8ac70f8be3dff17513
chuxin <15115359055@139.com>
2024-10-10 09:37:28 +0800

    feat: demo-youlai 用于学习youlai的技术栈~

---
+++ demo-youlai/src/main/java/com/chuxin/demoyoulai/DemoYoulaiApplication.java
@@ -0,0 +1,13 @@
+package com.chuxin.demoyoulai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYoulaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoYoulaiApplication.class, args);
+    }
+
+}
---
+++ demo-youlai/src/test/java/com/chuxin/demoyoulai/DemoYoulaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuxin.demoyoulai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoYoulaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
