e7db859b7a2d569ae42f0989231b753ccce8c038
chuxin <15115359055@139.com>
2024-10-10 17:34:35 +0800

    feat: 完善 spring boot 自动装配 config

---
+++ demo-youlai/src/main/java/com/chuxin/demoyoulai/config/exception/MyAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.chuxin.demoyoulai.config.exception;
+
+import com.chuxin.demoyoulai.common.result.ResultCode;
+import com.chuxin.demoyoulai.util.ResponseUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Spring Security访问异常处理器
+ *
+ * @author haoxr
+ * @since 2022/10/18
+ */
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        ResponseUtils.writeErrMsg(response, ResultCode.ACCESS_UNAUTHORIZED);
+    }
+}
