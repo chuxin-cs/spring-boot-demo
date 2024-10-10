1f4a7e7394d5b206d9e784ade04598752644a37e
chuxin <15115359055@139.com>
2024-10-10 17:34:54 +0800

    feat: 完善 common

---
+++ demo-youlai/src/main/java/com/chuxin/demoyoulai/common/result/IResultCode.java
@@ -0,0 +1,15 @@
+package com.chuxin.demoyoulai.common.result;
+
+/**
+ * 响应码接口
+ *
+ * @author Ray
+ * @since 2022/2/18
+ **/
+public interface IResultCode {
+
+    String getCode();
+
+    String getMsg();
+
+}
