98521e6dfd9270e4c360f004a3995c6d7f48db31
chuxin <15115359055@139.com>
2025-02-26 15:42:36 +0800

    feat: demo-login 用来实现登录和注册功能，实现的功能点如下： 1、提供 登录 和 注册 接口 2、密码加密 3、实现 JWT 返回 token

---
+++ demo-login/src/test/java/com/chuxin/demologin/DemoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuxin.demologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
